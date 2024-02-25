9cc499e657dd969da2583ddbe1e6aa233e594715
Elton A Rodrigues <eltonrodrigues61@gmail.com>
2024-02-25 14:21:45 -0300

    feat: add email notification

--- src/middleware/provider/confirmUser.ts
+++ src/middleware/provider/confirmUser.ts
@@ -1,4 +1,4 @@
-import { AdminGetUserCommand, CognitoIdentityProviderClient, InitiateAuthCommand, InitiateAuthCommandInput } from '@aws-sdk/client-cognito-identity-provider';
+import { ListUsersCommand, AdminGetUserCommand, CognitoIdentityProviderClient, InitiateAuthCommand, InitiateAuthCommandInput, ListUsersCommandInput } from '@aws-sdk/client-cognito-identity-provider';
 import { UserConfirmationData } from 'types/UserTypes';
 
 const AWS_REGION = process.env.COGNITO_REGION ?? 'us-east-1';
@@ -10,6 +10,13 @@ async function confirmUser(userData: UserConfirmationData) {
     Username: userData.Username,
   };
   const command = new AdminGetUserCommand(input);
+  return await client.send(command);
+}
+
+async function searchUserBySub(input: ListUsersCommandInput) {
+  const client = new CognitoIdentityProviderClient({ region: AWS_REGION });
+  console.log(input)
+  const command = new ListUsersCommand(input);
   const response = await client.send(command);
   return response;
 }
@@ -32,4 +39,4 @@ async function authClient(username: string, password: string) {
   return authResponse?.AuthenticationResult?.AccessToken;
 }
 
-export { authClient, confirmUser };
+export { authClient, confirmUser, searchUserBySub };
